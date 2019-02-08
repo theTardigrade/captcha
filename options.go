2ab951e1df7429640ff72a5c99c6fc0a4d80997b
James <jismith1989@googlemail.com>
2019-02-08 20:25:06 +0000

    Allow custom letter:number ratio

--- options.go
+++ options.go
@@ -3,10 +3,11 @@ package captcha
 import "image/color"
 
 const (
-	DefaultWidth                  = 800
-	DefaultHeight                 = 200
-	DefaultFontSize       float64 = 64
-	DefaultCharacterCount         = 7
+	DefaultWidth                       = 800
+	DefaultHeight                      = 200
+	DefaultFontSize            float64 = 64
+	DefaultCharacterCount              = 7
+	DefaultLetterToNumberRatio float64 = 0.7
 
 	defaultArea = DefaultWidth * DefaultHeight
 )
@@ -19,13 +20,14 @@ const (
 )
 
 type Options struct {
-	BackgroundColor color.RGBA
-	TextColor       color.RGBA
-	BackgroundType  backgroundType
-	Width, Height   int
-	FontSize        float64
-	CharacterCount  int
-	UseIdentifier   bool
+	BackgroundColor     color.RGBA
+	TextColor           color.RGBA
+	BackgroundType      backgroundType
+	Width, Height       int
+	FontSize            float64
+	CharacterCount      int
+	UseIdentifier       bool
+	LetterToNumberRatio float64
 }
 
 func (o *Options) SetDefaults() {
@@ -43,5 +45,9 @@ func (o *Options) SetDefaults() {
 
 	if o.CharacterCount == 0 {
 		o.CharacterCount = DefaultCharacterCount
+	}
+
+	if o.LetterToNumberRatio == 0 {
+		o.LetterToNumberRatio = DefaultLetterToNumberRatio
 	}
 }
