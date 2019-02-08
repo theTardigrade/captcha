fa4ddca1c7fec4d3c8116b59f3abd55a8c1329a2
James <jismith1989@googlemail.com>
2019-02-08 12:41:31 +0000

    Fix custom text colour

--- captcha.go
+++ captcha.go
@@ -102,7 +102,7 @@ func (c *Captcha) generateImage(opts *Options) error {
 		return err
 	}
 	dc.SetFontFace(font)
-	dc.SetRGBA(float64(textColor.R), float64(textColor.G), float64(textColor.B), 0.75)
+	dc.SetRGBA(float64(textColor.R)/255, float64(textColor.G)/255, float64(textColor.B)/255, 0.75)
 
 	for i := 0; i < characterCount; i++ {
 		var s string
