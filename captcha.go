3348f7271bcab8db9db4743eb1293cc53b6470e5
James <jismith1989@googlemail.com>
2019-02-08 11:47:03 +0000

    Improve background generation

--- captcha.go
+++ captcha.go
@@ -41,7 +41,7 @@ func New(opts Options) (*Captcha, error) {
 
 	r, g, b := float64(backgroundColor.R)/255, float64(backgroundColor.G)/255, float64(backgroundColor.B)/255
 
-	for x := float64(0); x < width; x += float64(rand.Intn(int(width/6))) + width/40 {
+	for x := float64(0); x < width; x += float64(rand.Intn(int(width/5)+1)) + width/80 {
 		a := float64(rand.Intn(49)+16) / 64
 		dc.SetRGBA(r, g, b, a)
 		r := float64(rand.Intn(int(area/1e3)+1)) + area/500
